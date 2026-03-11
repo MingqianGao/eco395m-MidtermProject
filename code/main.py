c2dc339642639d2c718ec5952b5cce047b2186d7
MingqianGao <gao_mingqian@163.com>
2026-03-11 17:07:48 -0500

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # ------------------------------#
 # 0. Paths
@@ -8,14 +11,19 @@ CODE = Path(__file__).resolve().parent
 ROOT = CODE.parent
 RAW = ROOT / "raw"
 INT = ROOT / "intermediate"
-OUT = ROOT / "output"
 DATA = ROOT / "data_cleaning"
+OUT = ROOT / "output"
 
 # ------------------------------#
 # 1. Descriptive Statistics
 # ------------------------------#
 
 df = pd.read_csv(DATA / "final_panel.csv")
+
+df['paper_per_capita'] = df['paper_writing'] / df['population']
+df["log_GDPperCapita"] = np.log(df["GDPperCapita"])
+df["log_urbanpop"] = np.log(df["urbanpop"])
+
 
 vars = [
     "paper_writing",
@@ -27,11 +35,29 @@ vars = [
     "population",
     "Manushare",
     "trade",
+"paper_per_capita",
 ]
 
 summary = df[vars].describe().T
 
+summary.to_csv(OUT / "summary_statistics.csv")
+
+# ----------------------------------------------------------#
+# Correlation Heatmap
+# ----------------------------------------------------------#
+
+
+corr = df[vars].corr()
+
+plt.figure(figsize=(8,6))
+sns.heatmap(corr, annot=True, cmap="coolwarm", fmt=".2f")
+plt.title("Feature Correlation Matrix")
+
+plt.savefig(OUT / "correlation_heatmap.png", dpi=300)
+plt.show()
+
 missing = df[vars].isnull().sum()
 
 summary.to_csv(OUT / "summary_statistics.csv")
+
 
